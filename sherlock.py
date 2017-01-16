95c3bff69488a04f776d7967136102d20d4a7842
cori <corigrohman@gmail.com>
2017-01-15 19:39:14 -0600

    updat: added LICENSE/env_config.py. Created first alchemy news request and capped out my free daily api ration in a matter of minutes... not cool

--- sherlock.py
+++ sherlock.py
@@ -1,9 +1,36 @@
+import os
 from pymongo import MongoClient
+import logging
+from logging.config import dictConfig
+import env_config
+import json
+from watson_developer_cloud import AlchemyDataNewsV1
 
+dictConfig(env_config.logging_config)
+logger = logging.getLogger()
 
 def main():
+	try:
+		alchemy_api=os.getenv("ALCHEMY_NEWS_API")
+		alchemy_url=os.getenv("ALCHEMY_NEWS_URL")
+	except:
+		logger.error('Could not find AlchemyNews env variables')
+	alchemy_data_news = AlchemyDataNewsV1(api_key=alchemy_api)
+	results = alchemy_data_news.get_news_documents(start='now-7d', end='now', time_slice='12h')
+	print(json.dumps(results, indent=2))
 
-	
+
+	results = alchemy_data_news.get_news_documents(
+	    start='now-2d',
+	    end='now',
+	    return_fields=['enriched.url.title',
+	                   'enriched.url.url',
+	                   'enriched.url.author',
+	                   'enriched.url.publicationDate'],
+	    query_fields={
+	        'q.enriched.url.enrichedTitle.entities.entity':
+	            '|text=Tesla,type=company|'})
+	print(json.dumps(results, indent=2))
 
 
 if __name__ =="__main__":
